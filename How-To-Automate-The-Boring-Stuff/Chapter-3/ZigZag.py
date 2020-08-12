e8b7c258f2374c3998ca4a0ec3b15bb5cd743286
ldelvill <36253281+ldelvill@users.noreply.github.com>
2020-08-11 19:14:18 -0700

    Add files via upload

---
+++ How-To-Automate-The-Boring-Stuff/Chapter-3/ZigZag.py
@@ -0,0 +1,24 @@
+# prgram that displays a zigzag pattern
+
+import time
+import sys
+
+indent = 0
+indentIncreasing = True
+
+try:
+    while True:
+        print(" " * indent, end="")
+        print("********")
+        time.sleep(0.1)
+
+        if indentIncreasing:
+            indent = indent + 1
+            if indent == 20:
+                indentIncreasing = False
+        else:
+            indent = indent - 1
+            if indent == 0:
+                indentIncreasing = True
+except KeyboardInterrupt:
+    sys.exit()
